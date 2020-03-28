54bae2e737954ff9a00c4bf14fb30587bcccd5fd
Simone Fontana <fontana.simone@outlook.com>
2020-03-28 10:58:59 +0100

    Fix centroid calculation for better numerical stability

--- metric/metric.hpp
+++ metric/metric.hpp
@@ -1,31 +1,29 @@
-#ifndef POINT_CLOUD_REGISTRATION_BENCHMARK_HPP
-#define POINT_CLOUD_REGISTRATION_BENCHMARK_HPP
+#ifndef POINT_CLOUD_REGISTRATION_BENCHMARK_HPP_
+#define POINT_CLOUD_REGISTRATION_BENCHMARK_HPP_
 
 #include <assert.h>
 
-#include <pcl/common/centroid.h>
-#include <pcl/common/distances.h>
-#include <pcl/common/geometry.h>
-#include <pcl/point_cloud.h>
-#include <pcl/point_types.h>
+#include "pcl/common/centroid.h"
+#include "pcl/common/geometry.h"
+#include "pcl/point_cloud.h"
 
 namespace point_cloud_registration_benchmark {
 
-inline double calculate_error(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud1,
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud2)
-{
-    assert(cloud1->size() == cloud2->size());
-    double error = 0;
-    pcl::PointXYZ centroid;
-    pcl::computeCentroid(*cloud1, centroid);
-    for (int i = 0; i < cloud1->size(); i++) {
-        double centroid_distance = pcl::geometry::distance(cloud1->at(i), centroid);
-        error += (pcl::euclideanDistance(cloud1->at(i), cloud2->at(i))/centroid_distance);
-    }
-    error /= cloud1->size();
-    return error;
+inline double calculate_error(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud1, pcl::PointCloud<pcl::PointXYZ>::Ptr cloud2) {
+  assert(cloud1->size() == cloud2->size());
+  double error = 0;
+  Eigen::Vector4d centroid_v;
+  pcl::compute3DCentroid(*cloud1, centroid_v);
+  pcl::PointXYZ centroid(centroid_v[0], centroid_v[1], centroid_v[2]);
+  for (int i = 0; i < cloud1->size(); i++) {
+    double centroid_distance = pcl::euclideanDistance(cloud1->at(i), centroid);
+
+    error += pcl::euclideanDistance(cloud1->at(i), cloud2->at(i)) / centroid_distance;
+  }
+  error /= cloud1->size();
+  return error;
 }
 
-} // namespace point_cloud_registration_benchmark
+}  // namespace point_cloud_registration_benchmark
 
-#endif
+#endif  // POINT_CLOUD_REGISTRATION_BENCHMARK_HPP_
